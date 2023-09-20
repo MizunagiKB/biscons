94a06a5838da032c2d328cbfe7dc2bc12f7f9489
MizunagiKB <mizukb@LIVE.JP>
2023-09-20 21:09:24 +0900

    win32 python static to shared

--- env.windows-x86_64.bat
+++ env.windows-x86_64.bat
@@ -3,12 +3,14 @@ REM SPDX-FileCopyrightText: 2023 MizunagiKB <mizukb@live.jp>
 @echo off
 mkdir env\windows.x86_64
 pushd env\windows.x86_64
-if not exist "cpython-3.10.13+20230826-x86_64-pc-windows-msvc-static-install_only.tar.gz" (
+if not exist "cpython-3.10.13+20230826-x86_64-pc-windows-msvc-shared-install_only.tar.gz" (
     echo install python
-    curl -LkSsO https://github.com/indygreg/python-build-standalone/releases/download/20230826/cpython-3.10.13+20230826-x86_64-pc-windows-msvc-static-install_only.tar.gz
-    tar xf cpython-3.10.13+20230826-x86_64-pc-windows-msvc-static-install_only.tar.gz
+    curl -LkSsO https://github.com/indygreg/python-build-standalone/releases/download/20230826/cpython-3.10.13+20230826-x86_64-pc-windows-msvc-shared-install_only.tar.gz
+    tar xf cpython-3.10.13+20230826-x86_64-pc-windows-msvc-shared-install_only.tar.gz
     python\python -m ensurepip
     python\python.exe -m pip install scons
     python\python.exe -m pip install cx_Freeze
 )
 popd
+
+env\windows.x86_64\python\python.exe setup.py build
